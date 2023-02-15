673f42c0b8da7eff35b6c7a6b8c8ea3e8b3ceeaf
stxss <93729036+stxss@users.noreply.github.com>
2023-02-15 18:49:11 +0000

    Create layout for the necessary functions

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,67 @@
 # Class that represents the full linked list
 class LinkedList
+
+    # Instance initialization method
+    def initialize
+    end
+
+    # Method to add a new node containing a value, to the end of the list
+    def append(value)
+    end
+
+    # Method to add a new node containing a value, to the start of thelist
+    def prepend(value)
+
+    end
+
+    # Method to return the total number of nodes in the list
+    def size
+
+    end
+
+    # Method to return the first node in the list
+    def head
+
+    end
+
+    # Method to return the last node in the list
+    def tail
+
+    end
+
+    # Method to return a node at a given index
+    def at(index)
+
+    end
+
+    # Method to remove the last element from the list
+    def pop
+
+    end
+
+    # Method that returns true if the passed value is in the list, otherwise returns false
+    def contains?(value)
+
+    end
+
+    # Method that returns the index of the node containing value, or nil if not found
+    def find(value)
+
+    end
+
+    # Method to represent the list object as strings in a '( value ) -> ( value ) -> ( value ) -> nil' format
+    def to_s
+    end
+
+    # Extra credit part
+    # Method to insert a node at a given index, as long as value and index are provided
+    def insert_at(value, index)
+
+    end
+
+    # Method to remove a node at the given index
+    def remove_at(index)
+
+    end
+
 end
